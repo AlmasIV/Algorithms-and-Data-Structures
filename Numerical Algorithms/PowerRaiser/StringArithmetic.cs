ba19fbd2640df2de02277a01f12d783d03a01f60
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-01-09 21:47:11 +0500

    Rename to SumPositiveInts. And extract some logic to reuse it. Start implementing the MultiplyPositiveInts.

--- Numerical Algorithms/PowerRaiser/StringArithmetic.cs
+++ Numerical Algorithms/PowerRaiser/StringArithmetic.cs
@@ -8,7 +8,7 @@ internal static class StringArithmetic
 		{ '0', 0 }, { '1', 1 }, { '2', 2 }, { '3', 3 }, { '4', 4 }, { '5', 5 }, { '6', 6 }, { '7', 7 }, { '8', 8 }, { '9', 9 }
 	};
 	private static readonly string s_inputErrorMessage = "Inputs must contain only digits (special characters, whitespace and letters aren't allowed).";
-	internal static string SumStringPositiveInts(string number1, string number2)
+	internal static string SumPositiveInts(string number1, string number2)
 	{
 		if (number1 == "0")
 		{
@@ -18,8 +18,7 @@ internal static class StringArithmetic
 		{
 			return number1;
 		}
-		string longest = number1.Length > number2.Length ? number1 : number2;
-		string shortest = number2.Length < number1.Length ? number2 : number1;
+		(string longest, string shortest) = OrderByDescending(number1, number2);
 		StringBuilder result = new StringBuilder(longest.Length + 1);
 		int a, b, sum, remainder = 0, shortestCounter = shortest.Length - 1;
 
@@ -41,6 +40,20 @@ internal static class StringArithmetic
 			result.Append(remainder);
 		}
 		return new string(result.ToString().Reverse().ToArray());
+	}
+	internal static string MultiplyPositiveInts(string number1, string number2)
+	{
+		if (number1 == "0" || number2 == "0")
+		{
+			return "0";
+		}
+		return "";
+	}
+	private static (string, string) OrderByDescending(string number1, string number2) {
+		return (
+			number1.Length > number2.Length ? number1 : number2,
+			number2.Length < number1.Length ? number2 : number1
+		);
 	}
 	private static bool IsValidDigit(char digit)
 	{
--- Numerical Algorithms/Tests/PowerRaiser.Tests/StringArithmeticTests.cs
+++ Numerical Algorithms/Tests/PowerRaiser.Tests/StringArithmeticTests.cs
@@ -9,22 +9,22 @@ public class StringArithmeticTests
 	[DataRow("0", "0")]
 	[DataRow("0", "1")]
 	[DataRow("1", "0")]
-	public void SumStringPositiveInts_SumNumberWithZero_ReturnsTheNumber(string number1, string number2)
+	public void SumPositiveInts_SumNumberWithZero_ReturnsTheNumber(string number1, string number2)
 	{
 		string expected = number1 == "0" ? number2 : number1;
 
-		string actual = StringArithmetic.SumStringPositiveInts(number1, number2);
+		string actual = StringArithmetic.SumPositiveInts(number1, number2);
 
 		Assert.AreEqual(expected, actual);
 	}
 
 	[TestMethod()]
 	[DataRow("18446744073709551615", "18446744073709551615")]
-	public void SumStringPositiveInts_SumMaxULongIntegers_ReturnsTheSum(string num1, string num2)
+	public void SumPositiveInts_SumMaxULongIntegers_ReturnsTheSum(string num1, string num2)
 	{
 		string expected = "36893488147419103230";
 
-		string actual = StringArithmetic.SumStringPositiveInts(num1, num2);
+		string actual = StringArithmetic.SumPositiveInts(num1, num2);
 
 		Assert.AreEqual(expected, actual);
 	}
