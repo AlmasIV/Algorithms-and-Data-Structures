d6ad22c36c827ab7a9787cd9c7b1eb5e1374a094
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-01-09 21:29:25 +0500

    Make the test class public (internal doesn't work). Introduce test with big numbers.

--- Numerical Algorithms/Tests/PowerRaiser.Tests/StringArithmeticTests.cs
+++ Numerical Algorithms/Tests/PowerRaiser.Tests/StringArithmeticTests.cs
@@ -3,15 +3,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PowerRaiser.Tests;
 
 [TestClass()]
-internal class StringArithmeticTests {
+public class StringArithmeticTests {
 	[TestMethod()]
 	[DataRow("0", "0")]
 	[DataRow("0", "1")]
 	[DataRow("1", "0")]
-	internal void SumStringNumbers_SumNumberWithZero_Number(string number1, string number2) {
+	public void SumStringNumbers_SumNumberWithZero_ReturnsTheNumber(string number1, string number2) {
 		string expected = number1 == "0" ? number2 : number1;
 
 		string actual = StringArithmetic.SumStringNumbers(number1, number2);
+
+		Assert.AreEqual(expected, actual);
+	}
+
+	[TestMethod()]
+	[DataRow("18446744073709551615", "18446744073709551615")]
+	public void SumStringNumbers_SumMaxULongIntegers_ReturnsTheSum(string num1, string num2) {
+		string expected = "36893488147419103230";
+
+		string actual = StringArithmetic.SumStringNumbers(num1, num2);
 
 		Assert.AreEqual(expected, actual);
 	}
