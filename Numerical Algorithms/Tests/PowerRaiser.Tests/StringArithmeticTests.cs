ea25d2c5baf279d427f93cd81e486e9d564334cd
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-01-21 19:47:30 +0500

    Expected is passed as an argument.

--- Numerical Algorithms/Tests/PowerRaiser.Tests/StringArithmeticTests.cs
+++ Numerical Algorithms/Tests/PowerRaiser.Tests/StringArithmeticTests.cs
@@ -6,58 +6,48 @@ namespace PowerRaiser.Tests;
 public class StringArithmeticTests
 {
 	[TestMethod()]
-	[DataRow("0", "0")]
-	[DataRow("0", "1")]
-	[DataRow("1", "0")]
-	public void SumPositiveInts_SumNumberWithZero_ReturnsTheNumber(string number1, string number2)
+	[DataRow("0", "0", "0")]
+	[DataRow("0", "1", "1")]
+	[DataRow("1", "0", "1")]
+	public void SumPositiveInts_SumNumberWithZero_ReturnsTheNumber(string number1, string number2, string expected)
 	{
-		string expected = number1 == "0" ? number2 : number1;
-
 		string actual = StringArithmetic.SumPositiveInts(number1, number2);
 
 		Assert.AreEqual(expected, actual);
 	}
 
 	[TestMethod()]
-	[DataRow("18446744073709551615", "18446744073709551615")]
-	public void SumPositiveInts_SumMaxULongIntegers_ReturnsTheSum(string num1, string num2)
+	[DataRow("18446744073709551615", "18446744073709551615", "36893488147419103230")]
+	public void SumPositiveInts_SumMaxULongIntegers_ReturnsTheSum(string num1, string num2, string expected)
 	{
-		string expected = "36893488147419103230";
-
 		string actual = StringArithmetic.SumPositiveInts(num1, num2);
 
 		Assert.AreEqual(expected, actual);
 	}
 
 	[TestMethod()]
-	[DataRow("0", "0")]
-	[DataRow("0", "1")]
-	[DataRow("1", "0")]
-	public void MultiplyPositiveInts_MultiplyByZero_ReturnsZero(string num1, string num2) {
-		string expected = "0";
-
+	[DataRow("0", "0", "0")]
+	[DataRow("0", "1", "0")]
+	[DataRow("1", "0", "0")]
+	public void MultiplyPositiveInts_MultiplyByZero_ReturnsZero(string num1, string num2, string expected) {
 		string actual = StringArithmetic.MultiplyPositiveInts(num1, num2);
 
 		Assert.AreEqual(expected, actual);
 	}
 
 	[TestMethod()]
-	[DataRow("1", "1")]
-	[DataRow("1", "100")]
-	[DataRow("100", "1")]
-	public void MultiplyPositiveInts_NaturalNumberMultipliedToOne_ReturnsNaturalNumber(string num1, string num2) {
-		string expected = num1 == "1" ? num2 : num1;
-
+	[DataRow("1", "1", "1")]
+	[DataRow("1", "100", "100")]
+	[DataRow("100", "1", "100")]
+	public void MultiplyPositiveInts_NaturalNumberMultipliedToOne_ReturnsNaturalNumber(string num1, string num2, string expected) {
 		string actual = StringArithmetic.MultiplyPositiveInts(num1, num2);
 
 		Assert.AreEqual(expected, actual);
 	}
 
 	[TestMethod()]
-	[DataRow("18446744073709551615", "18446744073709551615")]
-	public void MultiplyPositiveInts_MultiplyMaxULongNumbers_ReturnsTheProduct(string num1, string num2) {
-		string expected = "340282366920938463426481119284349108225";
-
+	[DataRow("18446744073709551615", "18446744073709551615", "340282366920938463426481119284349108225")]
+	public void MultiplyPositiveInts_MultiplyMaxULongNumbers_ReturnsTheProduct(string num1, string num2, string expected) {
 		string actual = StringArithmetic.MultiplyPositiveInts(num1, num2);
 		
 		Assert.AreEqual(expected, actual);
