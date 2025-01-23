26a68aa74ae1ca42e2972f50df0d404553caa1fd
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-01-23 22:11:25 +0500

    Implement prime finder.

---
+++ Numerical Algorithms/SieveOfEratosthenes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SieveOfEratosthenes;
+
+class Program
+{
+    static void Main()
+    {
+        ulong upperBound = 10000;
+        List<ulong> primes = PrimeFinder.FindPrimesUpToUpperBound(upperBound);
+        
+        foreach(ulong prime in primes) {
+            Console.WriteLine(prime);
+        }
+    }
+}
