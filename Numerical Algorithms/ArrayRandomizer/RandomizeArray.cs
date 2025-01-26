1cc053ca27028283a4226127d0be385f7e6dbc6a
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-01-26 18:26:15 +0500

    Add XML comments.

--- Numerical Algorithms/ArrayRandomizer/RandomizeArray.cs
+++ Numerical Algorithms/ArrayRandomizer/RandomizeArray.cs
@@ -1,7 +1,22 @@
 namespace ArrayRandomizer;
 
+/// <summary>
+/// Provides a method to shuffle an array in place using the Fisher-Yates algorithm.
+/// </summary>
 public static class Randomizer
 {
+	/// <summary>
+	/// 	Shuffles the specified array in place.
+	/// </summary>
+	/// <typeparam name="T">
+	/// 	The type of elements in the array.
+	/// </typeparam>
+	/// <param name="array">
+	/// 	The array to be shuffled.
+	/// </param>
+	/// <remarks>
+	/// 	This method uses the Fisher-Yates algorithm to ensure a uniform shuffle.
+	/// </remarks>
 	public static void RandomizeArray<T>(T[] array)
 	{
 		int length = array.Length;
