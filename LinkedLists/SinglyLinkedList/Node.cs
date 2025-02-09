0394ec0b9d475b51cf660115057d194556a3d81c
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-09 15:43:53 +0500

    Add comments.

--- LinkedLists/SinglyLinkedList/Node.cs
+++ LinkedLists/SinglyLinkedList/Node.cs
@@ -7,7 +7,14 @@ namespace SinglyLinkedList;
 /// 	The type of the value that the node stores.
 /// </typeparam>
 public class Node<T> {
+	/// <summary>
+	/// 	Gets or sets the value of the node.
+	/// </summary>
 	public T? Value { get; set; }
+
+	/// <summary>
+	/// 	Gets or sets the next node.
+	/// </summary>
 	public Node<T>? Next { get; set; }
 
 	/// <summary>
