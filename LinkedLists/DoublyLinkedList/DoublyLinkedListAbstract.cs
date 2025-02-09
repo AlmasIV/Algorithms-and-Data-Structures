e34e129d9ccadfb11a902f8b6405372f049d6f10
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-09 15:45:15 +0500

    DoublyLinkedList version 1.

---
+++ LinkedLists/DoublyLinkedList/DoublyLinkedListAbstract.cs
@@ -0,0 +1,9 @@
+using SinglyLinkedList;
+
+namespace DoublyLinkedList;
+
+/// <summary>
+/// 	Defines an abstract base class for a doubly linked list structures.
+/// </summary>
+/// <inheritdoc />
+public abstract class DoublyLinkedList<T> : SinglyLinkedListAbstract<T> {}
