cc40128a600a8995352322b29f05431e76e0e0fa
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-10 22:15:00 +0500

    Remove comments.

--- LinkedLists/DoublyLinkedList/DoublyLinkedList.cs
+++ LinkedLists/DoublyLinkedList/DoublyLinkedList.cs
@@ -2,11 +2,11 @@ using SinglyLinkedList;
 
 namespace DoublyLinkedList;
 
-/// <summary>
-/// 	Defines an abstract base class for a doubly linked list structures.
-/// </summary>
-/// <inheritdoc />
 public class DoublyLinkedList<T> : SinglyLinkedList<T> {
 	protected new DoublyNode<T>? Head { get; set; }
 	protected DoublyNode<T>? Tail { get; set; }
+    public override void PrependNode(Node<T> node)
+    {
+        base.PrependNode(node);
+    }
 }
--- LinkedLists/DoublyLinkedList/DoublyNode.cs
+++ LinkedLists/DoublyLinkedList/DoublyNode.cs
@@ -1,21 +1,9 @@
 using SinglyLinkedList;
 
 namespace DoublyLinkedList;
-
-/// <summary>
-/// 	Represents a node of a doubly linked list.
-/// </summary>
-/// <inheritdoc />
 public class DoublyNode<T> : Node<T>
 {
-	/// <inheritdoc />
 	public DoublyNode(){}
-
-	/// <inheritdoc />
 	public DoublyNode(T value) : base(value) {}
-
-	/// <summary>
-	/// 	Gets or sets the previous node.
-	/// </summary>
 	public Node<T>? Previous { get; set; }
 }
