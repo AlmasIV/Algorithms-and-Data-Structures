c07552340281dea35cc2d2ee1a8dd8fcd383c110
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-10 22:49:54 +0500

    Append node is ready, not tested though.

---
+++ LinkedLists/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+
+namespace DoublyLinkedList;
+
+public class DoublyLinkedList<T> : IEnumerable<Node<T>> {
+	public ulong Length { get; private set; }
+	public Node<T>? Head { get; private set; }
+	public Node<T>? Tail { get; private set; }
+
+	public void AppendNode(Node<T> node) {
+		ArgumentNullException.ThrowIfNull(node);
+		if(Head is null) {
+			Head = node;
+			Tail = node;
+		}
+		else {
+			Tail!.Next = node;
+			node.Previous = Tail;
+			Tail = node;
+		}
+	}
+
+    public IEnumerator<Node<T>> GetEnumerator()
+    {
+		Node<T>? temp = Head;
+		while(temp is not null) {
+			yield return temp;
+			temp = temp.Next;
+		}
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
