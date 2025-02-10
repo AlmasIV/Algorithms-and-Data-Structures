3e231533c1d50deae77f0e38a3255b2823c73ba3
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-10 22:41:29 +0500

    Previous and Next nodes can't be set from the outside.

--- LinkedLists/DoublyLinkedList/Node.cs
+++ LinkedLists/DoublyLinkedList/Node.cs
@@ -3,8 +3,8 @@ namespace DoublyLinkedList;
 public class Node<T>
 {
 	public T? Value { get; set; }
-	public Node<T>? Next { get; set; }
-	public Node<T>? Previous { get; set; }
+	public Node<T>? Next { get; internal set; }
+	public Node<T>? Previous { get; internal set; }
 	public Node(){}
 	public Node(T? value) {
 		Value = value;
