1d733468c9fe9d1982fd3b4b3b5b1e18fbe02815
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-12 07:36:49 +0500

    Add one more validation step. Format.

--- LinkedLists/DoublyLinkedList/DoublyLinkedList.cs
+++ LinkedLists/DoublyLinkedList/DoublyLinkedList.cs
@@ -2,50 +2,71 @@ using System.Collections;
 
 namespace DoublyLinkedList;
 
-public class DoublyLinkedList<T> : IEnumerable<Node<T>> {
+public class DoublyLinkedList<T> : IEnumerable<Node<T>>
+{
 	public ulong Length { get; private set; }
 	public Node<T>? Head { get; private set; }
 	public Node<T>? Tail { get; private set; }
 
-	public void AppendNode(Node<T> node) {
+	public void AppendNode(Node<T> node)
+	{
 		ArgumentNullException.ThrowIfNull(node);
-		if(Head is null) {
+		if (!IsStandAloneNode(node))
+		{
+			throw new ArgumentException("The node belongs to another linked list.", nameof(node));
+		}
+		if (Head is null)
+		{
 			Head = node;
 			Tail = node;
 		}
-		else {
+		else
+		{
 			Tail!.Next = node;
 			node.Previous = Tail;
 			Tail = node;
 		}
-		Length ++;
+		Length++;
 	}
 
-	public void PrependNode(Node<T> node) {
+	public void PrependNode(Node<T> node)
+	{
 		ArgumentNullException.ThrowIfNull(node);
-		if(Head is null) {
+		if (!IsStandAloneNode(node))
+		{
+			throw new ArgumentException("The node belongs to another linked list.", nameof(node));
+		}
+		if (Head is null)
+		{
 			Head = node;
 			Tail = node;
 		}
-		else {
+		else
+		{
 			Head.Previous = node;
 			node.Next = Head;
 			Head = node;
 		}
-		Length ++;
+		Length++;
 	}
 
-    public IEnumerator<Node<T>> GetEnumerator()
-    {
+	private bool IsStandAloneNode(Node<T> node)
+	{
+		return node.Next is null && node.Previous is null;
+	}
+
+	public IEnumerator<Node<T>> GetEnumerator()
+	{
 		Node<T>? temp = Head;
-		while(temp is not null) {
+		while (temp is not null)
+		{
 			yield return temp;
 			temp = temp.Next;
 		}
-    }
+	}
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return GetEnumerator();
+	}
 }
