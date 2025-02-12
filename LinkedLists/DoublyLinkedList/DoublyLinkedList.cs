6abb799380b8d0d4209faafaff9b31be96db7edf
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-12 21:38:09 +0500

    Add new methods for the linked list.

--- LinkedLists/DoublyLinkedList/DoublyLinkedList.cs
+++ LinkedLists/DoublyLinkedList/DoublyLinkedList.cs
@@ -52,9 +52,79 @@ public class DoublyLinkedList<T> : IEnumerable<Node<T>>
 		Length++;
 	}
 
+	public void DeleteNodeByReference(Node<T> node)
+	{
+		ArgumentNullException.ThrowIfNull(node);
+		if (!IsStandAloneNode(node))
+		{
+			throw new ArgumentException("The node belongs to another linked list.", nameof(node));
+		}
+		if (Length > 0 && Head == node)
+		{
+			DeleteFirst();
+		}
+		else if (Tail == node)
+		{
+			DeleteLast();
+		}
+		else
+		{
+			Node<T>? temp = Head;
+			while (temp is not null)
+			{
+				if (temp.Next == node)
+				{
+					temp.Next = temp.Next.Next;
+					if (temp.Next is not null)
+					{
+						temp.Next.Previous = temp;
+					}
+					ResetNode(node);
+					Length--;
+					break;
+				}
+				temp = temp.Next;
+			}
+		}
+	}
+
+	public void DeleteFirst()
+	{
+		if (Head is not null)
+		{
+			Node<T> temp = Head;
+			Head = Head.Next;
+			if (Head is not null)
+			{
+				Head.Previous = null;
+			}
+			ResetNode(temp);
+			Length--;
+		}
+	}
+
+	public void DeleteLast()
+	{
+		if (Tail is not null)
+		{
+			Node<T> temp = Tail;
+			Tail = Tail.Previous;
+			ResetNode(temp);
+			Length --;
+		}
+	}
+
 	private bool IsStandAloneNode(Node<T> node)
 	{
 		return node.Next is null && node.Previous is null && node.LinkedListId is null;
+	}
+
+	private void ResetNode(Node<T> node)
+	{
+		node.Previous = null;
+		node.Next = null;
+		node.Value = default;
+		node.LinkedListId = null;
 	}
 
 	public IEnumerator<Node<T>> GetEnumerator()
