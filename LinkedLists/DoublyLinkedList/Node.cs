21ebea6d68f7caf8c6e218f8b0de8209ffce0d5c
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-12 07:58:26 +0500

    Node can point to a linked list now.

--- LinkedLists/DoublyLinkedList/Node.cs
+++ LinkedLists/DoublyLinkedList/Node.cs
@@ -3,8 +3,9 @@ namespace DoublyLinkedList;
 public class Node<T>
 {
 	public T? Value { get; set; }
-	public Node<T>? Next { get; internal set; }
-	public Node<T>? Previous { get; internal set; }
+	public Node<T>? Next { get; internal set; } = null;
+	public Node<T>? Previous { get; internal set; } = null;
+	public Guid? LinkedListId { get; internal set; } = null;
 	public Node(){}
 	public Node(T? value) {
 		Value = value;
