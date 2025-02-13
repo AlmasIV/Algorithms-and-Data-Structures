38a3c0ba6d80d4229f2ad51367ff3b4a28524469
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-13 08:09:10 +0500

    Delete last tests are added.

---
+++ LinkedLists/Tests/DoublyLinkedList.Tests/Tests/DeleteLast.cs
@@ -0,0 +1,87 @@
+namespace DoublyLinkedList.Tests;
+
+[TestClass()]
+public class DeleteLastTests
+{
+	private Func<DoublyLinkedList<Guid>> InitializeNonEmptyLinkedList = SUTInitializer.InitializeNonEmptyLinkedList;
+
+	[TestMethod()]
+	public void DeleteLast_DeletingOnEmptyLinkedList_DoesNothing()
+	{
+		DoublyLinkedList<Guid> linkedList = new DoublyLinkedList<Guid>();
+		ulong expectedLength = linkedList.Length;
+
+		linkedList.DeleteLast();
+		ulong actualLength = linkedList.Length;
+
+		Assert.AreEqual(expectedLength, actualLength);
+	}
+
+	[TestMethod()]
+	public void DeleteLast_DeletingOnNonEmptyLinkedList_DecrementsLength()
+	{
+		DoublyLinkedList<Guid> linkedList = InitializeNonEmptyLinkedList();
+		ulong expectedLength = linkedList.Length - 1;
+
+		linkedList.DeleteLast();
+		ulong actualLength = linkedList.Length;
+
+		Assert.AreEqual(expectedLength, actualLength);
+	}
+
+	[TestMethod()]
+	public void DeleteLast_DeletingOnNonEmptyLinkedList_DeletesLastNode()
+	{
+		DoublyLinkedList<Guid> linkedList = InitializeNonEmptyLinkedList();
+		Node<Guid> tailNode = linkedList.Tail!;
+
+		linkedList.DeleteLast();
+		Node<Guid> newTailNode = linkedList.Tail!;
+
+		Assert.AreNotEqual(tailNode, newTailNode);
+	}
+
+	[TestMethod()]
+	public void DeleteLast_DeletingOnNonEmptyLinkedList_DeletedNodeNextIsNull()
+	{
+		DoublyLinkedList<Guid> linkedList = InitializeNonEmptyLinkedList();
+		Node<Guid> tailNode = linkedList.Tail!;
+
+		linkedList.DeleteLast();
+
+		Assert.IsNull(tailNode.Next);
+	}
+
+	[TestMethod()]
+	public void DeleteLast_DeletingOnNonEmptyLinkedList_DeletedNodePreviousIsNull()
+	{
+		DoublyLinkedList<Guid> linkedList = InitializeNonEmptyLinkedList();
+		Node<Guid> tailNode = linkedList.Tail!;
+
+		linkedList.DeleteLast();
+
+		Assert.IsNull(tailNode.Previous);
+	}
+
+	[TestMethod()]
+	public void DeleteLast_DeletingOnNonEmptyLinkedList_DeletedNodeLinkedListIdIsDefault()
+	{
+		DoublyLinkedList<Guid> linkedList = InitializeNonEmptyLinkedList();
+		Node<Guid> tailNode = linkedList.Tail!;
+
+		linkedList.DeleteLast();
+
+		Assert.AreEqual(tailNode.LinkedListId, default);
+	}
+
+	[TestMethod()]
+	public void DeleteLast_DeletingOnNonEmptyLinkedList_DeletedNodeValueIsDefault()
+	{
+		DoublyLinkedList<Guid> linkedList = InitializeNonEmptyLinkedList();
+		Node<Guid> tailNode = linkedList.Tail!;
+
+		linkedList.DeleteLast();
+
+		Assert.AreEqual(tailNode.Value, default);
+	}
+}
