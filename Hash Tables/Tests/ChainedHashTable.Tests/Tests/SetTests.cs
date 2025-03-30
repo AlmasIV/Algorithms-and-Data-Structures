42549d4f59867732aa8cad0734b5ed7d164ef092
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-03-30 21:29:56 +0500

    Corrected a test.

--- Hash Tables/Tests/ChainedHashTable.Tests/Tests/SetTests.cs
+++ Hash Tables/Tests/ChainedHashTable.Tests/Tests/SetTests.cs
@@ -35,17 +35,17 @@ public class SetTests
 	[DataRow(1)]
 	[DataRow(10)]
 	[DataRow(90)]
-	public void Set_SetItems_Works(int count)
+	public void Set_SetItems_ItemsAreActuallySet(int count)
 	{
 		KeyValuePair<int, string>[] keyValuePairs = GetRandomData(count);
 		ChainedHashTable<int, string> myHashTable = new ChainedHashTable<int, string>();
-		int expected = keyValuePairs.Length;
+		bool expected = true;
 
 		foreach (KeyValuePair<int, string> keyValuePair in keyValuePairs)
 		{
 			myHashTable.Set(keyValuePair.Key, keyValuePair.Value);
 		}
-		int actual = myHashTable.Count();
+		bool actual = ContainsIdentitcalItems(keyValuePairs, myHashTable.ToArray());
 
 		Assert.AreEqual(expected, actual);
 	}
@@ -58,10 +58,31 @@ public class SetTests
 		{
 			int key = random.Next();
 			KeyValuePair<int, string> keyValuePair = new KeyValuePair<int, string>(key, $"Test Number {key}.");
-			if(!keyValuePairs.Any(pair => pair.Key == key)) {
+			if (!keyValuePairs.Any(pair => pair.Key == key))
+			{
 				keyValuePairs[i] = keyValuePair;
 			}
 		}
 		return keyValuePairs;
+	}
+
+	private bool ContainsIdentitcalItems(KeyValuePair<int, string>[] keyValuePairs1, KeyValuePair<int, string>[] keyValuePairs2)
+	{
+		if (keyValuePairs1.Length != keyValuePairs2.Length)
+		{
+			return false;
+		}
+
+		KeyValuePair<int, string>[] sorted1 = keyValuePairs1.OrderBy(kv => kv.Key).ToArray();
+		KeyValuePair<int, string>[] sorted2 = keyValuePairs2.OrderBy(kv => kv.Key).ToArray();
+
+		for (int i = 0; i < sorted1.Length; i++)
+		{
+			if (!sorted1[i].Equals(sorted2[i]))
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 }
