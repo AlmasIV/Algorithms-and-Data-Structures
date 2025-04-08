a8470a1ed606fef720005f28c6341b7b0c72887a
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-04-08 15:38:29 +0500

    Add method is implemented for the LinearProbingHashTable.

--- Hash Tables/OpenAddressedHashTable/HashTable/LinearProbingHashTable.cs
+++ Hash Tables/OpenAddressedHashTable/HashTable/LinearProbingHashTable.cs
@@ -9,9 +9,34 @@ public class LinearProbingHashTable<K, V> : HashTableAbstract<K, V> where K : IC
 	{
 		ArgumentNullException.ThrowIfNull(key, nameof(key));
 		ArgumentNullException.ThrowIfNull(value, nameof(value));
-		
-		int hashedKey = _HashKey(key);
 
+		int hashedKey = _HashKey(key);
+		int index = hashedKey;
+		int firstTombstone = -1;
+		do
+		{
+			KeyValuePair<K, (bool, V)>? probe = _buckets[index];
+			if (probe.HasValue)
+			{
+				if (probe.Value.Value.Item1 is true)
+				{
+					firstTombstone = firstTombstone < 0 ? index : firstTombstone;
+				}
+				else if (probe.Value.Key.CompareTo(key) == 0)
+				{
+					throw new ArgumentException("The item with the specified key already exists!");
+				}
+
+				index = (index + 1) & (_buckets.Length - 1);
+			}
+			else
+			{
+				break;
+			}
+		} while (hashedKey != index);
+
+		index = firstTombstone < 0 ? index : firstTombstone;
+		_buckets[index] = new KeyValuePair<K, (bool, V)>(key, (false, value));
 
 		Count++;
 		if (_loadFactor >= 0.75)
